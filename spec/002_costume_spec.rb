c4d74a4c7b27cd3aaa305abb977fac7e601ed63a
Katie Hoffman <ktahoffman@gmail.com>
2014-10-20 15:23:57 -0400

    merge with master, pass specs for new model, costume

--- spec/002_costume_spec.rb
+++ spec/002_costume_spec.rb
@@ -6,7 +6,7 @@ describe "Costume" do
     hot_dog = Costume.create(
       name: "Unisex Adult Hot Dog Costume"
     )
-    expect(CostumeStore.find_by(name: "Unisex Adult Hot Dog Costume")).to eq(hot_dog)
+    expect(Costume.find_by(name: "Unisex Adult Hot Dog Costume")).to eq(hot_dog)
   end
 
   it "has a price" do
@@ -14,25 +14,25 @@ describe "Costume" do
       name: "Men's Grand Heritage Caribbean Pirate",
       price: 169.97
     )
-    expect(CostumeStore.find_by(price: 169.97)).to eq(pirate)
+    expect(Costume.find_by(price: 169.97)).to eq(pirate)
   end
 
   it "has an image url" do
     url = "http://img.costumecraze.com/images/vendors/rasta/7139-large.jpg"
-    penguin = CostumeStore.create(
+    penguin = Costume.create(
       name: "Happy Penguin",
       image_url: url
     )
-    expect(CostumeStore.find_by(image_url: url)).to eq(penguin)
+    expect(Costume.find_by(image_url: url)).to eq(penguin)
   end
 
   it "has a size" do
-    horse = CostumeStore.create(
+    horse = Costume.create(
       name: "Horse Mask",
       image_url: "http://a.tgcdn.net/images/products/zoom/ec82_horse_head_mask.jpg",
       size: "medium"
     )
-    expect(CostumeStore.find_by(size: "medium")).to eq(horse)
+    expect(Costume.find_by(size: "medium")).to eq(horse)
   end
 
 end
