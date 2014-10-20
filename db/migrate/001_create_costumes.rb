2bcc9d9d4f2af8c8ae2f2934d2957ade9ea8ceaa
Katie Hoffman <ktahoffman@gmail.com>
2014-10-20 15:23:57 -0400

    merge with master, pass specs for new model, costume

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,9 +1,12 @@
-# Create a class and inherit from ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.string :size
+      t.string :image_url
+      t.float :price
 
-# By convention, the class name should match the part of the
-# file name after the number, so in this case:
-# 002_create_costumes.rb becomes class CreateCostumes
-
-# Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
-# value types according to the spec
+      t.timestamps
+    end
+  end
+end
