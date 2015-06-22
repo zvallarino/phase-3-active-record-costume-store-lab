acf47da7fef805e4a986d77a8777d9b5b6d7d2bb
Rose Weixel <rose.weixel@gmail.com>
2015-06-22 17:11:53 -0400

    add null: false

---
+++ db/migrations/003_create_haunted_houses.rb
@@ -0,0 +1,16 @@
+class CreateHauntedHouses < ActiveRecord::Migration
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.float :price
+      t.string :theme
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+      
+      t.timestamps null: false
+    end
+  end
+end
