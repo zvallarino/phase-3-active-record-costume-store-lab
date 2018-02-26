a4930a4b1a056feb7384843f94cef04d56c0df2e
curiositypaths <curiositypaths@users.noreply.github.com>
2018-02-26 13:49:03 -0500

    Add the ActiveRecord::Migration version declaration to migrations

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,16 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.float :price
+      t.string :theme
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+      
+      t.timestamps null: false
+    end
+  end
+end
