edb54af8230a87b86e90e502037b5476a7e8ff43
maxwellbenton <maxwellbenton@gmail.com>
2019-07-17 17:19:55 -0400

    update RSpec config +mirror neighbors -system call

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,23 @@
 ENV["ACTIVE_RECORD_ENV"] = "test"
 
 require_relative '../config/environment'
+require 'rake'
+load './Rakefile'
 
 RSpec.configure do |config|
 
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
   config.before(:suite) do
-    clear_db
-    system("rake db:migrate")
-    DB.tables
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 
 end
